83b74f255efa916077a4283055dd47ca72fdbbb3
Anwesh43 <anweshthecool0@gmail.com>
2020-11-07 04:58:11 +0530

    adding hooks

--- src/hooks.js
+++ src/hooks.js
@@ -36,12 +36,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w, 
         h
+    }
+}
+
+const sinify = (scale) => Math.sin(scale * Math.PI)
+
+export const useStyle = (w, h, scale) => {
+    const size = Math.min(w, h) / 10
+    const position = 'absolute'
+    const x = w / 2
+    const y = h / 2
+    const lineWidth = Math.min(w, h) / 90
+    const background = 'indigo'
+    return {
+        parentStyle() {
+            const left = `${x}px`
+            const top = `${y}px`
+            return {
+                left,
+                top,
+                position
+            }
+        },
+        midLineStyle() {
+          const width = `${lineWidth}px`
+          const left = `${-wSize / 2}px`
+          const top = `${-size / 2}px`
+          const height = `${size}px`
+          return {
+              background, 
+              left, 
+              top, 
+              position,
+              width, 
+              height
+          }
+        },
+        hlineStyle(i) {
+            const top = `${-size / 2 + size * i}px`
+            const left = `${-size / 2}px`
+            const width = `${size}px`
+            const height = `${lineWidth}px`
+            return {
+                position,
+                left, 
+                top,
+                width,
+                height,
+                background
+            }
+        }         
     }
 }
